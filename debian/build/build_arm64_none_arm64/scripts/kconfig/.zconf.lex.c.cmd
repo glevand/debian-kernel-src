216f9e68a8d13e91294ff5cf38e99dab7c1168cf
Geoff Levand <geoff@infradead.org>
2018-06-12 18:13:49 -0700

    Run debian/rules.gen

---
+++ debian/build/build_arm64_none_arm64/scripts/kconfig/.zconf.lex.c.cmd
@@ -0,0 +1,1 @@
+cmd_scripts/kconfig/zconf.lex.c := flex -oscripts/kconfig/zconf.lex.c -L /work/build-1/linux-4.16.12/scripts/kconfig/zconf.l
---
+++ debian/build/build_arm64_none_arm64/scripts/kconfig/.zconf.tab.c.cmd
@@ -0,0 +1,1 @@
+cmd_scripts/kconfig/zconf.tab.c := bison -oscripts/kconfig/zconf.tab.c -t -l /work/build-1/linux-4.16.12/scripts/kconfig/zconf.y
